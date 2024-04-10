a6ca30075024e93ea66cea53f21f0982f68615ca
Albin Ryberg <alry23@student.bth.se>
2024-04-10 13:17:09 +0200

    added sass to project

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,10 @@
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
+ * 
+ *  php -S localhost:8888 -t public
  */
-import './styles/app.css';
+import './styles/app.scss';
 import hello from './js/hello.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -69,10 +69,10 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 
-    // .copyFiles({
-    //     from: './assets/images',
-    //     to: 'images/[path][name].[ext]'
-    // })
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]'
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
