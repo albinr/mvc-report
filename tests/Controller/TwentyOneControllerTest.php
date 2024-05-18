12f5ecf454578245b4ca9301f7155c8b693c07dc
Albin Ryberg <alry23@student.bth.se>
2024-05-18 16:52:56 +0200

    Added test for gamecontroller

--- tests/Controller/TwentyOneControllerTest.php
+++ tests/Controller/TwentyOneControllerTest.php
@@ -7,52 +7,54 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class TwentyOneControllerTest extends WebTestCase
 {
-    public function testHome()
+    public function testGame()
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', '/game');
 
         $this->assertResponseIsSuccessful();
         $this->assertStringContainsString('<!DOCTYPE html>', $client->getResponse()->getContent());
     }
 
-    public function testAbout()
+    public function testStart()
     {
         $client = static::createClient();
-        $client->request('GET', '/about');
+        $client->request('GET', '/game/start');
 
         $this->assertResponseIsSuccessful();
     }
 
 
-    public function testReport()
+    public function testHit()
     {
         $client = static::createClient();
-        $client->request('GET', '/report');
+        $client->request('GET', '/game/start');
+        $client->request('GET', '/game/hit');
 
         $this->assertResponseIsSuccessful();
     }
 
-    public function testLucky()
+    public function testStand()
     {
         $client = static::createClient();
-        $client->request('GET', '/lucky');
+        $client->request('GET', '/game/start');
+        $client->request('GET', '/game/stand');
 
         $this->assertResponseIsSuccessful();
     }
 
-    public function testMetrics()
+    public function testDoc()
     {
         $client = static::createClient();
-        $client->request('GET', '/metrics');
+        $client->request('GET', '/game/doc');
 
         $this->assertResponseIsSuccessful();
     }
 
-    public function testSession()
+    public function testApiGameStatus()
     {
         $client = static::createClient();
-        $client->request('GET', '/session');
+        $client->request('GET', '/api/game');
 
         $this->assertResponseIsSuccessful();
     }
