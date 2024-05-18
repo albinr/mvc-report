543134b640e0a6949e0408b6eb83f5c791c7f3d5
Albin Ryberg <alry23@student.bth.se>
2024-05-18 18:21:44 +0200

    fixed tests for librarycontroller

--- tests/Controller/LibraryControllerTest.php
+++ tests/Controller/LibraryControllerTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LibraryControllerTest extends WebTestCase
 {
@@ -48,6 +47,4 @@ class LibraryControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
     }
-
-
 }
